eafe96922835dc89e96092788d34fbcc173cd15f
Diego F. Gomez <gomezdiego1998@gmail.com>
2017-09-15 19:24:15 -0500

    Inicio de Proyecto MVC

---
+++ ProyectoSoftware2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoSoftware2.Startup))]
+namespace ProyectoSoftware2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
